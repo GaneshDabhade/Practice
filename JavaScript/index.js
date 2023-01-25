42d966aff334ec5ca0423ca160bbd574b3ad1ada
GaneshDabhade <54856733+GaneshDabhade@users.noreply.github.com>
2023-01-26 00:30:39 +0530

    Add files via upload

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,19 +1,51 @@
 let leadsList = []
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("leadsList"))
 let saveInputBtn = document.getElementById("saveInput-btn")
+let saveTabBtn = document.getElementById("saveTab-btn")
+let deleteBtn = document.getElementById("delete-btn")
 let inputBox = document.getElementById("inputBox-el")
 let ulEl = document.getElementById("ul-el")
 
+if(leadsFromLocalStorage){
+    leadsList = leadsFromLocalStorage
+    render(leadsFromLocalStorage)
+}
+
 saveInputBtn.addEventListener("click", function(){   
-    leadsList.push(inputBox.value) 
-    console.log(leadsList)
+    let inputValue = inputBox.value
+    if(inputValue !== ""){
+        leadsList.push(inputBox.value) 
+        saveToLocalStorage(leadsList)
+        render(leadsList)
+    }
+});
+
+saveTabBtn.addEventListener("click", function(){
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var currentTab = tabs[0];
+        var currentTabUrl = currentTab.url;
+        leadsList.push(currentTabUrl)
+        saveToLocalStorage(leadsList)
+        render(leadsList)
+    });
+});
+
+deleteBtn.addEventListener("click", function(){
+    leadsList = []
+    localStorage.clear()
     render(leadsList)
 })
+
+function saveToLocalStorage(leads){
+    localStorage.setItem("leadsList", JSON.stringify(leads))
+}
 
 function render(leads){
     let listItems = ""
     for(let i = 0; i < leads.length; i++){
         listItems += `<li>${leads[i]}</li>`
     }   
+    console.log(listItems)
     ulEl.innerHTML = listItems
 }
 
